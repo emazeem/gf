002712d8ba38443025735da04691147fc49352a5
emazeem <62165761+emazeem@users.noreply.github.com>
2022-08-09 00:26:16 +0500

    aub

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -21,31 +21,9 @@
             <div class="card">
                 <div class="card-body">
                     <!-- title -->
-                    <div class="d-md-flex">
-                        <div>
-                            <h4 class="card-title">Halls </h4>
-                            <h5 class="card-subtitle">Overview of all halls bookings</h5>
-                        </div>
-                    </div>
                     <!-- title -->
                 </div>
-                <div class="table-responsive">
-                    <table class="table v-middle">
-                        <thead>
-                        <tr class="bg-light">
-                            <th class="border-top-0">Hall</th>
-                            <th class="border-top-0">Marriage Hall</th>
-                            <th class="border-top-0">Location</th>
-                            <th class="border-top-0">Capacity</th>
-                            <th class="border-top-0">Price</th>
-                            <th class="border-top-0">Today Status</th>
-                        </tr>
-                        </thead>
-                        <tbody>
 
-                        </tbody>
-                    </table>
-                </div>
             </div>
         </div>
     </div>
