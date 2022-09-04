43ceb0028cd44a865a9cce08682c1480cc0b2998
emazeem <62165761+emazeem@users.noreply.github.com>
2022-09-04 22:44:40 +0500

    mian

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user=new User();
+        $user->name='Admin';
+        $user->email='admin@gmail.com';
+        $user->email_verified_at='2022-04-28 08:49:09';
+        $user->password='$2y$10$SFiswqS6p5Y3WquEDod9qeTpnyMTpWeWT.7pvkolBUlymFi.fMXBW';
+        $user->username='emazeem0';
+        $user->timezone='';
+        $user->role='admin';
+        $user->status='active';
+        $user->stripe_customer_id=NULL;
+        $user->last_login=NULL;
+        $user->remember_token=NULL;
+        $user->created_at='2022-04-03 08:46:09';
+        $user->updated_at='2022-04-03 08:46:09';
+        $user->save();
+
+    }
+}
