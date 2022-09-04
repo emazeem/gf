db49f76f3e620b4c926021d6f0f00d1599dd4901
emazeem <62165761+emazeem@users.noreply.github.com>
2022-09-04 23:43:44 +0500

    naub

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Event;
 class EventServiceProvider extends ServiceProvider
 {
     /**
-     * The event to listener mappings for the application.
+     * The event listener mappings for the application.
      *
      * @var array<class-string, array<int, class-string>>
      */
@@ -18,7 +18,6 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
-
     ];
 
     /**
@@ -29,15 +28,5 @@ class EventServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-    }
-
-    /**
-     * Determine if events and listeners should be automatically discovered.
-     *
-     * @return bool
-     */
-    public function shouldDiscoverEvents()
-    {
-        return false;
     }
 }
